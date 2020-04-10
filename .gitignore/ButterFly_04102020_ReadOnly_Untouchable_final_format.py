e09323f676d37305eb9f70708b8e58dc42199538
liangwen12year <36004580+liangwen12year@users.noreply.github.com>
2020-04-10 12:42:27 -0400

    Add files via upload

---
+++ .gitignore/ButterFly_04102020_ReadOnly_Untouchable_final_format.py
@@ -0,0 +1,152 @@
+import re
+import csv
+import operator
+from collections import defaultdict
+import pandas as pd
+import sys
+def run():
+    count = 0
+    totalRow = 0
+    # 'Epitope-356214-Folate-Receptor-Alpha.csv'
+    if len(sys.argv) != 4:
+        print("missing or too many arguments!!!")
+        return
+    with open(sys.argv[1]) as csvDataFile:
+        csvReader = csv.reader(csvDataFile)
+        totalRow = sum(1 for _ in csvReader)
+        # print('total Row: ' + str(totalRow))
+
+    with open(sys.argv[1]) as csvDataFile:
+        csvReader = csv.reader(csvDataFile)
+        row1 = next(csvReader)
+        count += 1
+        file = open('temp.csv', 'w')
+        file.write('sample' +',' +'timepoint' + ',' +'percentd_replicate' + ',' +'start'+ ',' + 'end' + ',' + 'Peptide'+ '\n')
+
+
+        while ('0s' not in row1) and ('.raw' not in row1):
+            row1 = next(csvReader)
+            count += 1
+        cycle = 1
+        peptideCheckFlag = int(sys.argv[3])
+        while row1 != '':
+            # print(row1)
+            if cycle == 1:
+                peptideCheck = row1[0]
+            if peptideCheckFlag == 1:
+                #print('*********')
+                if peptideCheck != row1[0]:
+                    print('++++++++++')
+                    peptideCheckFlag = 0
+                    print("Wrong Row: " + str(count) + '   ' + row1[2] + '-' + row1[3])
+            if 'false' in row1:
+                # print('***********************************')
+                indexOfBracket = row1[0].find('[')
+                peptide = row1[0][:indexOfBracket]
+                if indexOfBracket == -1:
+                    peptide = row1[0]
+                # print(row1[20] + ',' + row1[21] + ',' + row1[35] + ',' + row1[2] + ',' + row1[3] + ',' + peptide )
+                file.write(row1[20] + ',' + row1[21] + ',' + row1[35] + ',' + row1[2] + ',' + row1[3] + ',' + peptide +'\n' )
+            if count == totalRow:
+                break
+            row1 = next(csvReader)
+            count += 1
+            cycle += 1
+            if cycle == 1 + int(sys.argv[2]):
+                cycle = 1
+                    # print(count)
+
+    file.close()
+
+
+    with open('temp.csv') as csvDataFile2:
+        csvReader2 = csv.reader(csvDataFile2)
+        tempRow = next(csvReader2)
+        sortedlist = sorted(csvReader2, key=lambda row: (int(row[3]), int(row[4])), reverse=False)
+        dictres = defaultdict(list)
+        timepointDic = {}
+        peptideList = []
+        for row in sortedlist:
+            timepointDic[row[1]] = timepointDic.get(row[1], 0) + 1
+            key = row[0] +'-'+ row[1]
+            if row[1] == '0s':
+                continue
+                # dictres[key] = dictres.get(key, []) + [row[3] + '--' + row[4]]
+            elif(row[1] == '15s'):
+                peptideList.append(row[5])
+                Key0s = row[0] +'-' + '0s'
+                dictres[Key0s] = dictres.get(Key0s, []) + [row[3] + '--' + row[4]]
+                dictres[key] = dictres.get(key, []) + [row[2]]
+            else:
+                dictres[key] = dictres.get(key, []) + [row[2]]
+        res = dict(dictres)
+        keylist  = list(res.keys())
+        print('total key: '+ str(len(keylist)))
+        print(keylist)
+
+        header = ''
+        for j in range(len(keylist)):
+            if (j == len(keylist)/2):
+                continue
+            if j == 0:
+                header = header + "Peptide Range" + ','
+            else:
+                header = header + keylist[j] +','
+
+        header = header + ',' + ',' + ',' + 'Peptide Range(Differential)' +','
+
+
+        timepointList = list(timepointDic.keys())
+        print(timepointDic)
+
+        for k in range(1, len(timepointDic)):
+            header = header + timepointList[k] + ','
+
+
+        header = header + 'Peptide' + ',' + 'Dmax' + ',' + ',' + ',' + 'Peptide Range(Differential/Da)' +','
+        for k in range(1, len(timepointDic)):
+            header = header + timepointList[k] + ','
+
+        for k in range(1, len(timepointDic)-1):
+            print(timepointDic[timepointList[k]])
+            if timepointDic[timepointList[k]] != timepointDic[timepointList[k+1]]:
+                return 99
+
+        outputFileName = 'Deuterium Exchange Plot_' + sys.argv[1]
+        file2 = open(outputFileName, 'w')
+        file2.write(header)
+        file2.write('\n')
+        print(keylist)
+        for i in range(len(res[keylist[0]])):
+            finalres = ''
+            for j in range(len(keylist)):
+                if (j == len(keylist)/2):
+                    continue
+                if j < len(keylist)//2 :
+                    finalres = finalres + res[keylist[j]][i] + ','
+                else:
+                    finalres = finalres + '-' +res[keylist[j]][i] + ','
+            finalres = finalres +',' + ',' +',' + res[keylist[0]][i] +','
+            DaDiffList = []
+            for x in range(1,len(keylist)//2):
+                DaDiffList.append(float(res[keylist[x + len(keylist)//2]][i]) - float(res[keylist[x]][i]))
+                finalres = finalres + str(float(res[keylist[x + len(keylist)//2]][i]) - float(res[keylist[x]][i])) + ','
+            finalres += peptideList[2*i]+','
+            Dmax = len((peptideList[2*i][2:len(peptideList[2*i])]).replace('P', ''))
+            finalres += str(Dmax)
+            finalres = finalres + ',' + ',' + ',' + res[keylist[0]][i] + ','
+            for p in range(len(DaDiffList)):
+                finalres = finalres + str(DaDiffList[p]*Dmax/100) +','
+
+            file2.write(finalres)
+            file2.write('\n')
+
+  
+
+    file2.close()
+    return 0
+  
+
+
+
+run()
